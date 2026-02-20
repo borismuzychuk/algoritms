2194b5a37926d5aa809aa16d49aae28d6478f130
Boris <bmuzychuk@textback.io>
2026-02-20 16:33:16 +0300

    Reverse Linked List

---
+++ src/main/java/org/muzychuk/boris/lists/ReversingLinkedList.java
@@ -0,0 +1,17 @@
+package org.muzychuk.boris.lists;
+
+public class ReversingLinkedList {
+
+    public ListNode reverseList(ListNode head) {
+        ListNode prev = null;
+        ListNode curr = head;
+        while (curr != null) {
+            ListNode temp = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = temp;
+        }
+        return prev;
+    }
+
+}
