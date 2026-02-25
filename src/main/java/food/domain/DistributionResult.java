0d76bc6708b948847e087d41cac433ece6f60b49
Boris <bmuzychuk@textback.io>
2026-02-25 14:21:07 +0300

    total for DistributionResult

--- src/main/java/food/domain/DistributionResult.java
+++ src/main/java/food/domain/DistributionResult.java
@@ -4,4 +4,10 @@ import java.math.BigDecimal;
 import java.util.Map;
 
 public record DistributionResult(Map<Partner, BigDecimal> result) {
+
+    public BigDecimal total() {
+        // todo
+        return null;
+    }
+
 }
