c1254a117c637771fef1fbf7cf497d955112f1b2
Музычук Борис Олегович <playmate7885@gmail.com>
2026-05-14 15:42:18 +0300

    RecursionMaxElement

--- src/main/java/org/muzychuk/boris/recursion/MaxElement.java
+++ src/main/java/org/muzychuk/boris/recursion/MaxElement.java
@@ -8,7 +8,6 @@ public class MaxElement {
         if (nums.isEmpty()) {
             throw new IllegalArgumentException("List nums is empty");
         }
-        int max = 0;
         if (nums.size() == 1) {
             return nums.getFirst();
         } else {
