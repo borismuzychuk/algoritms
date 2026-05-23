54747c04d8df5e7bfdcb5b921753ac6f9ca1d8ec
Музычук Борис Олегович <playmate7885@gmail.com>
2026-05-23 12:18:32 +0300

    BinarySearch recursive

--- src/main/java/org/muzychuk/boris/BinarySearch.java
+++ src/main/java/org/muzychuk/boris/BinarySearch.java
@@ -1,11 +1,14 @@
 package org.muzychuk.boris;
 
+import java.util.List;
+import java.util.Objects;
+
 public class BinarySearch {
 
     public int search(int[] nums, int target) {
         int min = 0;
         int max = nums.length - 1;
-        while(min <= max) {
+        while (min <= max) {
             int mid = (min + max) / 2;
             if (nums[mid] == target) {
                 return mid;
@@ -18,4 +21,43 @@ public class BinarySearch {
         }
         return -1;
     }
+
+    public int searchRecursive(List<Integer> nums, Integer target) {
+        return searchRecursive(nums, target, 0, nums.size() - 1);
+    }
+
+    public int searchRecursive(List<Integer> nums, Integer target, int left, int right) {
+        if (left > right) {
+            return -1;
+        }
+        int mid = left + (right - left) / 2;
+
+        if (Objects.equals(nums.get(mid), target)) {
+            return mid;
+        } else if (nums.get(mid) > target) {
+            return searchRecursive(nums, target, left, mid - 1);
+        } else {
+            return searchRecursive(nums, target, mid + 1, right);
+        }
+    }
+
+    public int searchRecursive(int[] nums, int target) {
+        return searchRecursive(nums, target, 0, nums.length - 1);
+    }
+
+    private int searchRecursive(int[] nums, int target, int left, int right) {
+        if (left > right) {
+            return -1;
+        }
+        int mid = left + (right - left) / 2;
+        if (nums[mid] == target) {
+            return mid;
+        } else if (nums[mid] > target) {
+            return searchRecursive(nums, target, left, mid - 1);
+        } else {
+            return searchRecursive(nums, target, mid + 1, right);
+        }
+    }
+
+
 }
--- src/test/java/org/muzychuk/boris/BinarySearchTest.java
+++ src/test/java/org/muzychuk/boris/BinarySearchTest.java
@@ -2,6 +2,9 @@ package org.muzychuk.boris;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class BinarySearchTest {
@@ -32,5 +35,18 @@ class BinarySearchTest {
         BinarySearch binarySearch = new BinarySearch();
         int result = binarySearch.search(new int[]{-2, -1, 0, 1, 2, 4, 6, 8}, -2);
         assertEquals(0, result);
+    }
+
+    @Test
+    void searchRecursiveList() {
+        BinarySearch binarySearch = new BinarySearch();
+        List<Integer> nums = List.of(-2, -1, 0, 1, 2, 4, 6, 8);
+        assertEquals(5, binarySearch.searchRecursive(nums, 4));
+    }
+
+    @Test
+    void searchRecursive() {
+        BinarySearch binarySearch = new BinarySearch();
+        assertEquals(12, binarySearch.searchRecursive(new int[]{-42, -15, -10, -8, -2, -1, 0, 1, 2, 4, 6, 8, 11, 13, 14, 42}, 11));
     }
 }
